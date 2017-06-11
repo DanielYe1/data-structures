6c384774171654e91570e2bf1315514dda021593
Daniel Ye <danielxinxin1@gmail.com>
2017-06-11 18:23:07 -0300

    implemented method to check if the graph is oriented or not

--- trab1/graph_filter.c
+++ trab1/graph_filter.c
@@ -2,15 +2,20 @@
 #include <stdlib.h>
 #include "graph.c"
 
+
+
+
 int main() {
     FILE *arq = fopen("trab1/arquivo.txt", "r");
     if (!arq) {
         exit(1);
     }
 
+    // lendo total de nós
     int total_nodes;
     fscanf(arq, "%d", &total_nodes);
     printf("%d\n", total_nodes);
+
 
     TG *graph = cria();
     // inserindo nós
@@ -18,17 +23,37 @@ int main() {
         insere_no(graph, i + 1);
     }
 
-    imprime(graph);
+    // inserindo arestas
 
-
+    int *aux = malloc(sizeof(int)*total_nodes);
     int from, to;
     while (fscanf(arq, "%d %d", &from, &to) != EOF) {
+        aux[from-1]+=to;
+        aux[to-1]-=from;
         insere_aresta(graph, from, to, 1);
         printf("%d %d\n", from, to);
     }
 
-    imprime(graph);
+    for (int k = 0; k < total_nodes; k++) {
+        printf("%d ",aux[k]);
+    }
 
+    int oriented=1;
+    for (int j = 0; j < total_nodes; j++) {
+        if(aux[j]){
+            oriented=0;
+            break;
+        }
+    }
+    if(oriented){
+        printf("aux");
+    }else{
+        printf("nao aux");
+    }
     fclose(arq);
+
+
+    free(aux);
+
     return 0;
 }
