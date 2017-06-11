52922691d074c999c342dc2792b7956bc7225b62
Daniel Ye <danielxinxin1@gmail.com>
2017-06-11 16:36:18 -0300

    creating graph with file input

--- trab1/graph_filter.c
+++ trab1/graph_filter.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "graph.c"
 
 int main() {
     FILE *arq = fopen("trab1/arquivo.txt", "r");
@@ -11,10 +12,22 @@ int main() {
     fscanf(arq, "%d", &total_nodes);
     printf("%d\n", total_nodes);
 
+    TG *graph = cria();
+    // inserindo nós
+    for (int i = 0; i < total_nodes; ++i) {
+        insere_no(graph, i + 1);
+    }
+
+    imprime(graph);
+
+
     int from, to;
     while (fscanf(arq, "%d %d", &from, &to) != EOF) {
+        insere_aresta(graph, from, to, 1);
         printf("%d %d\n", from, to);
     }
+
+    imprime(graph);
 
     fclose(arq);
     return 0;
