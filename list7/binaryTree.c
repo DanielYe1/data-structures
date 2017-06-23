bbd75dcd5c9ccbe195c86587188c05b73de472c1
Daniel Ye <danielxinxin1@gmail.com>
2017-06-22 21:20:27 -0300

    implemented method equals to binary tree

--- list7/binaryTree.c
+++ list7/binaryTree.c
@@ -39,3 +39,16 @@ TAB *espelho(TAB *a){
     }
     return novo;
 }
+
+int igual(TAB *a, TAB *b){
+    if(!a && !b){
+        return 1;
+    }
+    if(!a || !b){
+        return 0;
+    }
+    if(a->info == b->info){
+        return igual(a->dir,b->dir) && igual(a->esq,b->esq);
+    }
+    return 0;
+}
---
+++ list7/q6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "binaryTree.c"
+
+int main() {
+    TAB *t = cria(4);
+    t->dir = cria(1);
+    t->esq = cria(5);
+    t->dir->dir = cria(2);
+    t->esq->esq = cria(3);
+
+    TAB *b = cria(4);
+    b->dir = cria(1);
+    b->esq = cria(5);
+    b->dir->dir = cria(2);
+    b->esq->esq = cria(3);
+
+
+    printf("%d",igual(t,b));
+    return 0;
+}
